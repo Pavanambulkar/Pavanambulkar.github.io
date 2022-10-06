b506a4035dcb8f2c67ea66b414e0d7ffd40f5644
pavan <pavanambulkar69@gmail.com>
2022-10-06 15:26:47 +0530

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -11,8 +11,8 @@ const Home = () => {
       className="h-screen w-full bg-gradient-to-b 	bg-slate-300	 from-white via-skin to-white-800"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col mt-7 justify-center h-full">
-          <h2 className="text-4xl  sm:text-7xl font-bold text-pink-700 ">
+        <div className="flex flex-col mt-20 justify-center h-full">
+          <h2 className="text-4xl mt-20 sm:text-7xl font-bold text-pink-700 ">
             I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
@@ -35,12 +35,12 @@ const Home = () => {
           </a>
         </div>
 
-        <div>
+        <div >
           <img
             src={HeroImage}
             alt="my profile"
             // className="rounded-2xl mx-auto w-2/3 md:w-full"
-            className="rounded-full  mx-auto w-2/3 md:w-40px"
+            className="rounded-full mt-20  mx-auto w-2/3 md:w-40px"
             // className="h-14 bg-gradient-to-r from-purple-500 to-pink-500"
           />
         </div>
